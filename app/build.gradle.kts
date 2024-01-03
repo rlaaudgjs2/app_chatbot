1dc955e8fa6704a12fb87d5f2bed52619170bcbc
rlaaudgjs2 <68803644+rlaaudgjs2@users.noreply.github.com>
2024-01-03 14:49:24 +0900

    사이드 바 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,8 @@ android {
 }
 
 dependencies {
+    implementation ("androidx.drawerlayout:drawerlayout:1.1.1")
+    implementation ("androidx.appcompat:appcompat:1.3.1")
     implementation ("com.squareup.okhttp3:okhttp:4.10.0")
     implementation ("com.google.gms:google-services:4.3.15")
     implementation ("com.google.firebase:firebase-auth:22.0.0")
@@ -43,6 +45,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.drawerlayout:drawerlayout:1.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
